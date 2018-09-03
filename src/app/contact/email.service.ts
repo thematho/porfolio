cc1b5a1f24ec817699bd405ccbf15cfa7acf7ae6
Matias Fernandez Martinez <thematho@gmail.com>
2018-09-03 10:37:29 -0300

    Uncommented email key services Fixed unncessary click event to send email

--- src/app/contact/email.service.ts
+++ src/app/contact/email.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class EmailService {
   readonly destination: string = 'thematho@gmail.com';
-  readonly service_id: string = "default_serviceXXXXXX";
-  readonly template_id: string = "template_N6IOcP7qXXX";
+  readonly service_id: string = "default_service";
+  readonly template_id: string = "template_N6IOcP7q";
   emailjs = window['emailjs'];
 
   sendEmail(emailInfo: any): PromiseLike<Object> {
