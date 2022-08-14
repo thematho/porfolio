d08be642e8b0e300b75e2b4462bdaf2a5618cf31
Matias Fernandez Martinez <thematho@gmail.com>
2022-08-14 22:47:18 +0200

    Updated Angular 14

--- src/app/contact/business-contact/business-contact.component.ts
+++ src/app/contact/business-contact/business-contact.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { OverlayService } from './../../overlay.service';
 import { EmailService } from '../services/email/email.service';
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { UntypedFormControl, Validators, UntypedFormGroup, UntypedFormBuilder } from '@angular/forms';
 @Component({
   selector: 'business-contact',
   templateUrl: './business-contact.component.html',
   styleUrls: ['./business-contact.component.scss']
 })
 export class BusinessContactComponent implements OnInit {
-  contactForm: FormGroup;
+  contactForm: UntypedFormGroup;
   private emailService: EmailService;
   overlayService: OverlayService;
 
-  constructor(formBuilder: FormBuilder, emailService: EmailService, overlayService: OverlayService) {
+  constructor(formBuilder: UntypedFormBuilder, emailService: EmailService, overlayService: OverlayService) {
     this.contactForm = formBuilder.group({
-      'name': new FormControl('', [Validators.required]),
-      'email': new FormControl('', [Validators.required, Validators.email]),
-      'note': new FormControl(''),
+      'name': new UntypedFormControl('', [Validators.required]),
+      'email': new UntypedFormControl('', [Validators.required, Validators.email]),
+      'note': new UntypedFormControl(''),
     })
     this.emailService = emailService;
     this.overlayService = overlayService;
